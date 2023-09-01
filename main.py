05ba80c1780522f8b84c09fae8583a3d1299d2cf
Yab <yabuman46@gmail.com>
2023-09-01 16:30:14 +0300

    Created a flask app for serving it

---
+++ main.py
@@ -0,0 +1,31 @@
+import google.generativeai as palm
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from LLM import generate, init
+
+db, text_model = init()
+
+port_no = 5000
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/ask", methods=["GET", "POST"])
+def generate_response():
+    if request.method == "GET":
+        return "Please send POST request"
+
+    elif request.method == "POST":
+        query = request.json["query"]
+
+        response, passage = generate(text_model, query, db)
+
+        data = {"Response": response, "Context": passage}
+
+        return jsonify(data)
+
+
+print("Server running....")
+
+app.run(port=port_no)
