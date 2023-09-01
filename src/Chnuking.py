df43b92cc0b499aa9b0a5dc0d1d01f627720b1e5
Yab <yabuman46@gmail.com>
2023-09-01 18:02:15 +0300

    Added some more function to work on docx files

--- src/Chnuking.py
+++ src/Chnuking.py
@@ -22,3 +22,39 @@ def chunk_articles(paragraphs):
         articles.append(current_article.strip())
 
     return articles
+
+
+# Function to stro
+def read_document(filepath: str):
+    document: str = ""
+
+    if filepath.endswith(".docx"):
+        doc = Document(filepath)
+
+        for paragraph in doc.paragraphs:
+            document += paragraph.text + "\n"
+
+    else:
+        with open(filepath, "r") as file:
+            document = file.read()
+
+    documents = document.split("*****")
+
+    if ("Proclamation" in filepath) and documents:
+        procNum = documents[0].strip()
+
+        documents = documents[1:]
+        cited_docs = []
+        for document in documents:
+            document = f"{procNum} : \n {document} "
+            cited_docs.append(document)
+
+        documents = cited_docs
+
+        # This is only for debugging purpose only
+        # with open("temp.txt", "+a") as file:
+        #     for document in documents:
+        #         file.write(f"{document} \n\n*****\n\n")
+
+    # print(len(documents))  # For debugging only
+    return documents
